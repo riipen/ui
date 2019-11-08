b56e0d3bf11ffb4c63526ac0d61dad7636f904ae
Jordan Ell <me@jordanell.com>
2019-11-08 10:51:33 -0800

    Rebuild of documentation

--- packages/riipen-ui-docs/pages/components-api/headline.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import MarkdownPage from "src/modules/components/MarkdownPage";
-
-const req = require.context("src/pages/components-api", false, /headline.md$/);
-
-export default function Page() {
-  return <MarkdownPage path="pages/components-api/headline" req={req} />;
-}
--- packages/riipen-ui-docs/pages/components-api/text.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import MarkdownPage from "src/modules/components/MarkdownPage";
-
-const req = require.context("src/pages/components-api", false, /text.md$/);
-
-export default function Page() {
-  return <MarkdownPage path="pages/components-api/text" req={req} />;
-}
