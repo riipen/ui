ceb7df31e2181a79b2f7eb7d283830605033c3f3
Dustin Bolink <dustin.bolink@riipen.com>
2020-02-24 15:00:03 -0800

    [PLT-4660] Chip Component (#13)
    * Updated Chip Component

---
+++ packages/riipen-ui-docs/src/pages/components/chip/Disabled.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Chip from "@riipen-ui/components/Chip";
+
+export default function Disabled() {
+  const style = {
+    marginRight: "10px"
+  };
+
+  return (
+    <div>
+      <Chip label="Default" />
+      <span style={style} />
+      <Chip label="Default" disabled />
+    </div>
+  );
+}
