34f94093f4af8a7e2d1ab16743f29f79a314f9ba
bolinkd <dustin.bolink@riipen.com>
2020-02-27 11:01:51 -0800

    Review changes

--- packages/riipen-ui/src/utils/withClasses.jsx
+++ packages/riipen-ui/src/utils/withClasses.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import clsx from "clsx";
 import PropTypes from "prop-types";
 
 const withClasses = () => Component => {
-  if (process.env.NODE_ENV !== "production") {
-    if (Component === undefined) {
-      throw new Error(
-        [
-          "You are calling withStyles(styles)(Component) with an undefined component.",
-          "You may have forgotten to import it."
-        ].join("\n")
-      );
-    }
-  }
-
   const WithClasses = React.forwardRef(function WithClasses(props, ref) {
     const { classes, ...other } = props;
-    return (
-      <Component ref={ref} classes={[clsx("riipen", classes)]} {...other} />
-    );
+    return <Component ref={ref} classes={["riipen", ...classes]} {...other} />;
   });
+
+  WithClasses.defaultProps = {
+    classes: []
+  };
 
   WithClasses.propTypes = {
     /**
