cb1e3162c3078cdc03b041e18b2fd6512f0aefee
bolinkd <dustin.bolink@riipen.com>
2020-02-28 15:00:52 -0800

    updated prop types

--- packages/riipen-ui/src/utils/withClasses.jsx
+++ packages/riipen-ui/src/utils/withClasses.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const withClasses = WrappedComponent => {
   const handle = props => {
-    // eslint-disable-next-line react/prop-types
-    const { classes = [], ...other } = props;
+    const { classes, ...other } = props;
     return <WrappedComponent classes={["riipen", ...classes]} {...other} />;
+  };
+
+  handle.defaultProps = {
+    classes: []
+  };
+
+  handle.propTypes = {
+    /**
+     * Class list to append the default classes to.
+     */
+    classes: PropTypes.array
   };
 
   const name = WrappedComponent.displayName || WrappedComponent.name;
