8ebff8b0848c1b5df9f4ed1f2edcad8c556fdb44
Stephen Tredger <stredger@gmail.com>
2020-07-28 13:03:07 -0700

    Cleanup chip size prop docs

--- packages/riipen-ui-docs/src/pages/components/chip/Compact.jsx
+++ packages/riipen-ui-docs/src/pages/components/chip/Compact.jsx
@@ -7,30 +7,13 @@ export default function Compact() {
     marginRight: "10px"
   };
 
-  const breakStyle = {
-    marginTop: "10px"
-  };
-
   return (
     <div>
-      <div>
-        <Chip label="Default" size="small" color="default" />
-        <span style={style} />
-        <Chip label="Primary" size="small" color="primary" />
-        <span style={style} />
-        <Chip label="Secondary" size="small" color="secondary" />
-        <span style={style} />
-        <Chip label="Tertiary" size="small" color="tertiary" />
-      </div>
-      <div style={breakStyle}>
-        <Chip label="Positive" size="xsmall" color="positive" />
-        <span style={style} />
-        <Chip label="Warning" size="xsmall" color="warning" />
-        <span style={style} />
-        <Chip label="Negative" size="xsmall" color="negative" />
-        <span style={style} />
-        <Chip label="Dark" size="xsmall" color="dark" />
-      </div>
+      <Chip label="medium" size="medium" />
+      <span style={style} />
+      <Chip label="small" size="small" />
+      <span style={style} />
+      <Chip label="xsmall" size="xsmall" />
     </div>
   );
 }
