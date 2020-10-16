ec59fea1319093c3ab3015d90c52336fd59d5179
Stephen Tredger <stephen.tredger@riipen.com>
2020-10-16 14:04:46 -0700

    [PLT-5804] Refactor Form into function component (#80)
    * Refactor Form into function component
    
    * Remove rogue console.log
    
    * PR fixes
    
    * Bump ui version

--- packages/riipen-ui/src/components/Form.jsx
+++ packages/riipen-ui/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import css from "styled-jsx/css";
 
 import ThemeContext from "../styles/ThemeContext";
@@ -9,80 +9,37 @@ import withClasses from "../utils/withClasses";
 import Notice from "./Notice";
 import Typography from "./Typography";
 
-class Form extends React.Component {
-  static displayName = "Form";
+const isEmpty = value => {
+  return !value || !Object.keys(value).length;
+};
 
-  static propTypes = {
-    /**
-     * The content of the component.
-     */
-    children: PropTypes.node,
+const Form = ({
+  children,
+  classes,
+  enter,
+  error,
+  errors,
+  errorScroll,
+  ...other
+}) => {
+  const theme = React.useContext(ThemeContext);
 
-    /**
-     * An array of custom CSS classes to apply.
-     */
-    classes: PropTypes.array,
+  const errorRef = useRef(null);
 
-    /**
-     * Top level form error to display. One of either string or react node.
-     */
-    error: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-
-    /**
-     * A list of form errors to display. One of an object consisting of key value errors,
-     * an array of strings, or an array of react nodes.
-     */
-    errors: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.string),
-      PropTypes.arrayOf(PropTypes.element),
-      PropTypes.object
-    ]),
-
-    /**
-     * If `false`, error will not be scrolled to when it is passed to the form.
-     */
-    errorScroll: PropTypes.bool,
-
-    /**
-     * If `false`, pressing enter to submit the form will be disabled.
-     */
-    enter: PropTypes.bool
-  };
-
-  static defaultProps = {
-    classes: [],
-    enter: true,
-    errorScroll: true
-  };
-
-  componentDidUpdate(prevProps) {
-    const theme = this.context;
-
-    // If an error id dynamically added to the form, scroll the error notice in to view
-    if (
-      !prevProps.error &&
-      !prevProps.errors &&
-      (this.props.error || this.props.errors) &&
-      this.error &&
-      this.props.errorScroll
-    ) {
+  useEffect(() => {
+    if ((error || !isEmpty(errors)) && errorScroll) {
       const padding = theme.spacing(20);
-      const rect = this.error.getBoundingClientRect();
+
+      const rect = errorRef.current.getBoundingClientRect();
 
       // Only scroll if it's not already visible
       if (rect.y - padding < 0 || rect.y - padding > window.innerHeight) {
         window.scrollTo(0, rect.top + window.pageYOffset - padding);
       }
     }
-  }
+  }, [errorScroll, error, errors]);
 
-  setError = ref => {
-    this.error = ref;
-  };
-
-  getLinkedStyles = () => {
-    const theme = this.context;
-
+  const getLinkedStyles = () => {
     return css.resolve`
       .errorContainer {
         margin-bottom: ${theme.spacing(5)}px;
@@ -102,30 +59,26 @@ class Form extends React.Component {
     `;
   };
 
-  static contextType = ThemeContext;
-
-  handleKeyPress = e => {
-    const { enter } = this.props;
-
+  const handleKeyPress = e => {
     if (e.key === "Enter" && !enter) {
       e.stopPropagation();
       e.preventDefault();
     }
   };
 
-  renderErrorsString = errors => {
-    return errors.map((e, i) => (
+  const renderErrorsString = values => {
+    return values.map((e, i) => (
       <li key={i}>
         <Typography>{e}</Typography>
       </li>
     ));
   };
 
-  renderErrorsNode = errors => {
-    return errors.map((e, i) => <li key={i}>{e}</li>);
+  const renderErrorsNode = values => {
+    return values.map((e, i) => <li key={i}>{e}</li>);
   };
 
-  renderErrorsObject = errors => {
+  const renderErrorsObject = () => {
     const renderEntry = (key, message) => (
       <li key={key}>
         <Typography component="span">
@@ -163,82 +116,109 @@ class Form extends React.Component {
     return renderObject(errors);
   };
 
-  renderErrorNode = error => error;
-  renderErrorString = error => <Typography>{error}</Typography>;
+  const renderErrorNode = val => val;
+  const renderErrorString = val => <Typography>{val}</Typography>;
 
-  render() {
-    const { children, classes, error, errors, ...other } = this.props;
+  const linkedStyles = getLinkedStyles();
 
-    const linkedStyles = this.getLinkedStyles();
+  let errorRenderer;
+  let errorsRenderer;
 
-    const className = clsx(classes);
-
-    // Remove any unwanted props from "other"
-    delete other.enter;
-    delete other.errorScroll;
-
-    let errorRenderer;
-    let errorsRenderer;
-
-    if (error) {
-      if (typeof error === "string") {
-        errorRenderer = this.renderErrorString;
-      } else if (React.isValidElement(error)) {
-        errorRenderer = this.renderErrorNode;
-      }
+  if (error) {
+    if (typeof error === "string") {
+      errorRenderer = renderErrorString;
+    } else if (React.isValidElement(error)) {
+      errorRenderer = renderErrorNode;
     }
-
-    if (errors) {
-      if (
-        Array.isArray(errors) &&
-        errors.length > 0 &&
-        typeof errors[0] === "string"
-      ) {
-        errorsRenderer = this.renderErrorsString;
-      } else if (
-        Array.isArray(errors) &&
-        errors.length > 0 &&
-        React.isValidElement(errors[0])
-      ) {
-        errorsRenderer = this.renderErrorsNode;
-      } else if (typeof errors === "object" && Object.keys(errors).length > 0) {
-        errorsRenderer = this.renderErrorsObject;
-      }
-    }
-
-    return (
-      <React.Fragment>
-        {(errorRenderer || errorsRenderer) && (
-          <div
-            className={clsx(linkedStyles.className, "errorContainer")}
-            ref={this.setError}
-          >
-            <Notice classes={[linkedStyles.className]} color="negative">
-              {errorRenderer && (
-                <span className={clsx(linkedStyles.className, "error")}>
-                  {errorRenderer(error)}
-                </span>
-              )}
-              {errorsRenderer && (
-                <ul className={clsx(linkedStyles.className, "errors")}>
-                  {errorsRenderer(errors)}
-                </ul>
-              )}
-            </Notice>
-          </div>
-        )}
-        <form className={className} onKeyPress={this.handleKeyPress} {...other}>
-          {children}
-        </form>
-        <style jsx>{`
-          form {
-            width: 100%;
-          }
-        `}</style>
-        {linkedStyles.styles}
-      </React.Fragment>
-    );
   }
-}
+
+  if (!isEmpty(errors)) {
+    if (Array.isArray(errors) && typeof errors[0] === "string") {
+      errorsRenderer = renderErrorsString;
+    } else if (Array.isArray(errors) && React.isValidElement(errors[0])) {
+      errorsRenderer = renderErrorsNode;
+    } else if (typeof errors === "object") {
+      errorsRenderer = renderErrorsObject;
+    }
+  }
+
+  return (
+    <React.Fragment>
+      {(errorRenderer || errorsRenderer) && (
+        <div
+          className={clsx(linkedStyles.className, "errorContainer")}
+          ref={errorRef}
+        >
+          <Notice classes={[linkedStyles.className]} color="negative">
+            {errorRenderer && (
+              <span className={clsx(linkedStyles.className, "error")}>
+                {errorRenderer(error)}
+              </span>
+            )}
+            {errorsRenderer && (
+              <ul className={clsx(linkedStyles.className, "errors")}>
+                {errorsRenderer(errors)}
+              </ul>
+            )}
+          </Notice>
+        </div>
+      )}
+      <form className={clsx(classes)} onKeyPress={handleKeyPress} {...other}>
+        {children}
+      </form>
+      <style jsx>{`
+        form {
+          width: 100%;
+        }
+      `}</style>
+      {linkedStyles.styles}
+    </React.Fragment>
+  );
+};
+
+Form.displayName = "Form";
+
+Form.propTypes = {
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
+
+  /**
+   * An array of custom CSS classes to apply.
+   */
+  classes: PropTypes.array,
+
+  /**
+   * Top level form error to display. One of either string or react node.
+   */
+  error: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+
+  /**
+   * A list of form errors to display. One of an object consisting of key value errors,
+   * an array of strings, or an array of react nodes.
+   */
+  errors: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.object
+  ]),
+
+  /**
+   * If `false`, error will not be scrolled to when it is passed to the form.
+   */
+  errorScroll: PropTypes.bool,
+
+  /**
+   * If `false`, pressing enter to submit the form will be disabled.
+   */
+  enter: PropTypes.bool
+};
+
+Form.defaultProps = {
+  classes: [],
+  enter: true,
+  errorScroll: true
+};
 
 export default withClasses(Form);
