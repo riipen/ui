2c4eab1b75d035f471ea2678e9dad6a3afdcc5cc
Dustin Bolink <dustin.bolink@riipen.com>
2020-11-25 08:41:08 -0800

    [PLT-6320] Added radio buttons and groups (#93)
    * Added radio buttons and groups

---
+++ packages/riipen-ui-docs/src/pages/components/radio/Button.jsx
@@ -0,0 +1,50 @@
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+import Form from "riipen-ui/components/Form";
+import RadioButton from "riipen-ui/components/RadioButton";
+import RadioButtonGroup from "riipen-ui/components/RadioButtonGroup";
+
+const icon = i => <FontAwesomeIcon icon={i} />;
+
+export default function Button() {
+  const [state, setState] = React.useState("none");
+
+  const handleChange = (e, value) => setState(value);
+
+  return (
+    <Form>
+      <RadioButtonGroup
+        label="Variant"
+        hint="Pick your favorite variant."
+        onChange={handleChange}
+        required
+      >
+        <RadioButton
+          prefix={icon(faFlag)}
+          label="Prefix"
+          value="prefix"
+          checked={state.value === "prefix"}
+        />
+        <RadioButton
+          suffix={icon(faFlag)}
+          label="Suffix"
+          value="suffix"
+          checked={state.value === "suffix"}
+        />
+        <RadioButton
+          label="None"
+          value="none"
+          checked={state.value === "none"}
+        />
+        <RadioButton
+          disabled
+          label="Disabled"
+          value="disabled"
+          checked={state.value === "disabled"}
+        />
+      </RadioButtonGroup>
+    </Form>
+  );
+}
