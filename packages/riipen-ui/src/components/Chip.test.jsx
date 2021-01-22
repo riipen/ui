6beb57ddf5134c77cb4938ecae4259d9bd64abcd
licarm <43053496+licarm@users.noreply.github.com>
2021-01-22 15:30:12 -0800

    remove onIconClick prop from Chip (#108)
    * remove onIconClick prop from Chip
    
    * Chip test complete
    
    * removed onIconClick from ui docs
    
    * global afterEach in setup.js  that restores all mock functions after every test
    
    Co-authored-by: Carmen Li <carmen@Carmens-MacBook-Pro.local>
    Co-authored-by: Carmen Li <carmen@MacBook-Pro.hitronhub.home>

--- packages/riipen-ui/src/components/Chip.test.jsx
+++ packages/riipen-ui/src/components/Chip.test.jsx
@@ -12,6 +12,7 @@ describe("<Chip>", () => {
     } catch (e) {
       error = e;
     }
+
     expect(error).toEqual(undefined);
   });
 
@@ -34,32 +35,55 @@ describe("<Chip>", () => {
     });
   });
 
-  describe("variant prop", () => {
-    it("sets variant class name with a valid variant", () => {
-      const variant = "outlined";
-      const wrapper = mount(<Chip variant={variant} />);
+  describe("children prop", () => {
+    it("displays label from child", () => {
+      const label = "A label given as a child";
+      const child = <div>{label}</div>;
+
+      const wrapper = mount(<Chip>{child}</Chip>);
 
       expect(
         wrapper
-          .find("Chip")
-          .childAt(0)
-          .hasClass(variant)
+          .find(".label")
+          .children()
+          .contains(child)
       ).toEqual(true);
+      expect(
+        wrapper
+          .find(".label")
+          .childAt(0)
+          .text()
+      ).toEqual(label);
     });
   });
 
-  describe("component prop", () => {
-    it("sets component with valid custom component", () => {
-      const component = "a";
-      const wrapper = mount(<Chip component={component} />);
+  describe("classes prop", () => {
+    it("applies all classes to the root node of Chip", () => {
+      const classes = ["classOne", "classTwo"];
 
-      expect(wrapper.find(component)).toHaveLength(1);
+      const wrapper = mount(<Chip classes={classes} />);
+
       expect(
         wrapper
           .find("Chip")
           .childAt(0)
-          .name()
-      ).toEqual(component);
+          .hasClass(classes[0])
+      ).toEqual(true);
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass(classes[1])
+      ).toEqual(true);
+    });
+
+    it("gives an error when classes are provided as one string", () => {
+      const classes = "classOne classTwo";
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Chip classes={classes} />);
+
+      expect(errors).toHaveBeenCalled();
     });
   });
 
@@ -87,6 +111,112 @@ describe("<Chip>", () => {
     });
   });
 
+  describe("component prop", () => {
+    it("sets component with valid custom component", () => {
+      const component = "a";
+
+      const wrapper = mount(<Chip component={component} />);
+
+      expect(wrapper.find(component)).toHaveLength(1);
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .name()
+      ).toEqual(component);
+    });
+
+    it("gives an error when given an invalid component", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+      const component = "abcdefg";
+
+      mount(<Chip component={component} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("disabled prop", () => {
+    it("applies disabled class when disabled prop is true", () => {
+      const disabled = true;
+
+      const wrapper = mount(<Chip disabled={disabled} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass("disabled")
+      ).toEqual(true);
+    });
+
+    it("does not apply disabled class when disabled prop is false", () => {
+      const disabled = false;
+
+      const wrapper = mount(<Chip disabled={disabled} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass("disabled")
+      ).toEqual(false);
+    });
+  });
+
+  describe("hover prop", () => {
+    it("applies hover class when hover prop is true", () => {
+      const hover = true;
+
+      const wrapper = mount(<Chip hover={hover} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass("hover")
+      ).toEqual(true);
+    });
+
+    it("does not apply hover class when hover prop is false", () => {
+      const hover = false;
+
+      const wrapper = mount(<Chip hover={hover} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass("hover")
+      ).toEqual(false);
+    });
+  });
+
+  describe("icon prop", () => {
+    it("displays icon with valid custom elementType", () => {
+      const icon = "a";
+
+      const wrapper = mount(<Chip icon={icon} />);
+
+      expect(wrapper.find(icon)).toHaveLength(1);
+      expect(
+        wrapper
+          .find(".icon")
+          .childAt(0)
+          .name()
+      ).toEqual(icon);
+    });
+
+    it("gives an error given an invalid icon type", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+      const icon = "aaaaaaaaa";
+
+      mount(<Chip icon={icon} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+
   describe("label prop", () => {
     it("displays label from label prop", () => {
       const label = "A test label";
@@ -104,6 +234,7 @@ describe("<Chip>", () => {
     it("prioritizes displaying label over children", () => {
       const label = "label";
       const child = <div>"child"</div>;
+
       const wrapper = mount(<Chip label={label}>{child}</Chip>);
 
       expect(
@@ -121,28 +252,6 @@ describe("<Chip>", () => {
     });
   });
 
-  describe("children prop", () => {
-    it("displays label from child", () => {
-      const label = "A label given as a child";
-      const child = <div>{label}</div>;
-
-      const wrapper = mount(<Chip>{child}</Chip>);
-
-      expect(
-        wrapper
-          .find(".label")
-          .children()
-          .contains(child)
-      ).toEqual(true);
-      expect(
-        wrapper
-          .find(".label")
-          .childAt(0)
-          .text()
-      ).toEqual(label);
-    });
-  });
-
   describe("onClick prop", () => {
     it("invokes onClick handler when clicked", () => {
       const handler = jest.fn();
@@ -150,7 +259,56 @@ describe("<Chip>", () => {
       const wrapper = mount(<Chip onClick={handler} />);
 
       wrapper.find("Chip").simulate("click");
+
       expect(handler).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("size prop", () => {
+    it("sets size class name with a valid size", () => {
+      const size = "small";
+
+      const wrapper = mount(<Chip size={size} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass(size)
+      ).toEqual(true);
+    });
+
+    it("gives an error when given an invalid size", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+      const size = "large";
+
+      mount(<Chip size={size} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("variant prop", () => {
+    it("sets variant class name with a valid variant", () => {
+      const variant = "outlined";
+
+      const wrapper = mount(<Chip variant={variant} />);
+
+      expect(
+        wrapper
+          .find("Chip")
+          .childAt(0)
+          .hasClass(variant)
+      ).toEqual(true);
+    });
+
+    it("gives an error when given an invalid variant", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+      const variant = "somethingInvalid";
+
+      mount(<Chip variant={variant} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
     });
   });
 });
