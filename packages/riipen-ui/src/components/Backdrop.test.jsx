52a75f607219dbc0cafd4ea0b0757bf04c1496d6
licarm <43053496+licarm@users.noreply.github.com>
2021-01-26 09:31:23 -0800

    [PLT-6626] Unit tests for Backdrop (#114)
    * tests for Backdrop
    
    * check open prop
    
    * removed open prop from Backdrop and its tests
    
    Co-authored-by: Carmen Li <carmen@MacBook-Pro.hitronhub.home>

---
+++ packages/riipen-ui/src/components/Backdrop.test.jsx
@@ -0,0 +1,69 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import Backdrop from "./Backdrop";
+
+describe("<Backdrop>", () => {
+  it("renders without errors", () => {
+    let error;
+    try {
+      mount(<Backdrop />);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toEqual(undefined);
+  });
+
+  it("snapshot test", () => {
+    const wrapper = mount(<Backdrop />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  describe("children prop", () => {
+    it("has given child", () => {
+      const child = <h1>The children prop</h1>;
+
+      const wrapper = mount(<Backdrop children={child} />);
+
+      expect(
+        wrapper
+          .find("Backdrop")
+          .children()
+          .contains(child)
+      ).toEqual(true);
+    });
+  });
+
+  describe("classes prop", () => {
+    it("applies classes to the root node of Backdrop", () => {
+      const classes = ["classOne", "classTwo"];
+
+      const wrapper = mount(<Backdrop classes={classes} />);
+
+      expect(
+        wrapper
+          .find("Backdrop")
+          .childAt(0)
+          .hasClass(classes[0])
+      ).toEqual(true);
+      expect(
+        wrapper
+          .find("Backdrop")
+          .childAt(0)
+          .hasClass(classes[1])
+      ).toEqual(true);
+    });
+
+    it("gives an error when classes are provided as one string", () => {
+      const classes = "classOne classTwo";
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Backdrop classes={classes} />);
+
+      expect(errors).toHaveBeenCalled();
+    });
+  });
+});
