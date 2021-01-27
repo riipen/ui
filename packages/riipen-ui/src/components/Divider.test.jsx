f9a07eb3dbe314b18a58e130b4d37c9b7e09b276
Carmen Li <carmen@MacBook-Pro.hitronhub.home>
2021-01-27 12:21:48 -0800

    tests for Divider component

---
+++ packages/riipen-ui/src/components/Divider.test.jsx
@@ -0,0 +1,98 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import Divider from "./Divider";
+
+describe("<Divider>", () => {
+  it("renders without errors", () => {
+    let error;
+    try {
+      mount(<Divider />);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toEqual(undefined);
+  });
+
+  describe("default props", () => {
+    it("sets correct default props", () => {
+      const defaultProps = new Divider().type.defaultProps;
+
+      const wrapper = mount(<Divider />);
+
+      const component = wrapper.find("Divider");
+      expect(component.props().variant).toEqual(defaultProps.variant);
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("appends higher order values to default classes prop with withClass decorator", () => {
+      const classes = ["riipen", "riipen-divider"];
+
+      const wrapper = mount(<Divider />);
+
+      expect(
+        wrapper
+          .find("Divider")
+          .props()
+          .classes.sort()
+      ).toEqual(classes.sort());
+    });
+  });
+
+  describe("classes prop", () => {
+    it("applies all classes to the root node of Divider", () => {
+      const classes = ["classOne", "classTwo"];
+
+      const wrapper = mount(<Divider classes={classes} />);
+
+      expect(
+        wrapper
+          .find("Divider")
+          .childAt(0)
+          .hasClass(classes[0])
+      ).toEqual(true);
+      expect(
+        wrapper
+          .find("Divider")
+          .childAt(0)
+          .hasClass(classes[1])
+      ).toEqual(true);
+    });
+
+    it("gives an error when classes are provided as one string", () => {
+      const classes = "classOne classTwo";
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Divider classes={classes} />);
+
+      expect(errors).toHaveBeenCalled();
+    });
+  });
+
+  describe("variant prop", () => {
+    it("sets variant class name with a valid variant", () => {
+      const variant = "middle";
+
+      const wrapper = mount(<Divider variant={variant} />);
+
+      expect(
+        wrapper
+          .find("Divider")
+          .childAt(0)
+          .hasClass(variant)
+      ).toEqual(true);
+    });
+
+    it("gives an error when given an invalid variant", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+      const variant = "somethingInvalid";
+
+      mount(<Divider variant={variant} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+});
