fe096b525394ca77c6c3b4b22f6cd8ac4c57a60b
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2021-01-27 11:54:16 -0800

    [PLT-6625] Unit tests for Avatar (#116)
    * unit tests for Avatar
    
    * Updated tests
    
    Co-authored-by: Imogene McDonald <imogene@MacBook-Pro.hitronhub.home>

---
+++ packages/riipen-ui/src/components/Avatar.test.jsx
@@ -0,0 +1,140 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import Avatar from "./Avatar";
+
+describe("<Avatar>", () => {
+  it("renders without errors", () => {
+    let error;
+    try {
+      mount(<Avatar />);
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toEqual(undefined);
+  });
+
+  describe("default props", () => {
+    it("sets correct default props", () => {
+      const wrapper = mount(<Avatar />);
+
+      expect(wrapper.find("Avatar").props().size).toEqual("96px");
+      expect(wrapper.find("Avatar").props().variant).toEqual("circle");
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("appends higher order values to default classes prop with withClass decorator", () => {
+      const wrapper = mount(<Avatar />);
+
+      expect(wrapper.find("Avatar").props().classes).toEqual([
+        "riipen",
+        "riipen-avatar"
+      ]);
+    });
+  });
+
+  describe("alt prop", () => {
+    it("renders an img tag with alt value", () => {
+      const altVariant = "Test String";
+      const srcVariant = "/static/images/avatars/jordan-ell.png";
+
+      const wrapper = mount(<Avatar alt={altVariant} src={srcVariant} />);
+
+      expect(wrapper.find("img").props().alt).toEqual(altVariant);
+    });
+  });
+
+  describe("children prop", () => {
+    it("displays given children", () => {
+      const child = <div>ZZ</div>;
+
+      const wrapper = mount(<Avatar>{child}</Avatar>);
+
+      expect(
+        wrapper
+          .find(".inner")
+          .children()
+          .contains(child)
+      ).toEqual(true);
+    });
+  });
+
+  describe("classes prop", () => {
+    it("sets custom class", () => {
+      const classVariant = ["newClass"];
+
+      const wrapper = mount(<Avatar classes={classVariant} />);
+
+      expect(
+        wrapper
+          .find("Avatar")
+          .childAt(0)
+          .hasClass(classVariant[0])
+      ).toEqual(true);
+    });
+  });
+
+  describe("size prop", () => {
+    it("sets correct border size when size is above 70", () => {
+      const sizeVariant = "75px";
+
+      const wrapper = mount(<Avatar size={sizeVariant} />);
+
+      expect(
+        wrapper
+          .find("Avatar")
+          .instance()
+          .getBorderSize()
+      ).toEqual("4px");
+    });
+
+    it("sets correct border size when size is below 70", () => {
+      const sizeVariant = "65px";
+
+      const wrapper = mount(<Avatar size={sizeVariant} />);
+
+      expect(
+        wrapper
+          .find("Avatar")
+          .instance()
+          .getBorderSize()
+      ).toEqual("2px");
+    });
+  });
+
+  describe("src prop", () => {
+    it("renders an img tag with src value", () => {
+      const srcVariant = "/static/images/avatars/jordan-ell.png";
+
+      const wrapper = mount(<Avatar src={srcVariant} />);
+
+      expect(wrapper.find("Avatar").find("img")).toHaveLength(1);
+      expect(wrapper.find("img").props().src).toEqual(srcVariant);
+    });
+  });
+
+  describe("variant prop", () => {
+    it("sets variant class name with valid custom variant", () => {
+      const variant = "rounded";
+
+      const wrapper = mount(<Avatar variant={variant} />);
+
+      expect(
+        wrapper
+          .find("Avatar")
+          .childAt(0)
+          .hasClass(variant)
+      ).toEqual(true);
+    });
+
+    it("throws an error with with invalid variant", () => {
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Avatar variant="star" />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+});
