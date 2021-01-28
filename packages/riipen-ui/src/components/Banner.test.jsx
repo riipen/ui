c9f361777bc14b9b9f7d7b7d5a52ffb86b85f011
Carmen Li <carmen@MacBook-Pro.hitronhub.home>
2021-01-28 12:11:48 -0800

    children, classes, tint prop tests for Banner component

---
+++ packages/riipen-ui/src/components/Banner.test.jsx
@@ -0,0 +1,131 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import Banner from "./Banner.jsx";
+
+describe("<Banner>", () => {
+  it("renders without errors", () => {
+    let error;
+    try {
+      mount(<Banner />);
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toEqual(undefined);
+  });
+
+  it("renders correct snapshot", () => {
+    const wrapper = mount(<Banner />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  describe("children prop", () => {
+    it("renders given children", () => {
+      const child = <h5>hi hi</h5>;
+
+      const wrapper = mount(<Banner children={child} />);
+
+      expect(
+        wrapper
+          .find("Banner")
+          .childAt(0)
+          .contains(child)
+      ).toEqual(true);
+    });
+  });
+
+  describe("classes prop", () => {
+    it("applies all classes to the div inside of Banner", () => {
+      const classes = ["classOne", "classTwo"];
+
+      const wrapper = mount(<Banner classes={classes} />);
+
+      expect(
+        wrapper
+          .find("Banner")
+          .childAt(0)
+          .find("div")
+          .hasClass(classes[0])
+      ).toEqual(true);
+      expect(
+        wrapper
+          .find("Banner")
+          .childAt(0)
+          .find("div")
+          .hasClass(classes[1])
+      ).toEqual(true);
+    });
+
+    it("appends higher order values to classes prop with withClass decorator", () => {
+      const classes = ["riipen", "riipen-banner"];
+
+      const wrapper = mount(<Banner />);
+
+      expect(
+        wrapper
+          .find("Banner")
+          .props()
+          .classes.sort()
+      ).toEqual(classes.sort());
+    });
+
+    it("gives an error when classes are provided as one string", () => {
+      const classes = "classOne classTwo";
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Banner classes={classes} />);
+
+      expect(errors).toHaveBeenCalled();
+    });
+  });
+
+  describe("height prop", () => {
+    it("sets Banner to a given height", () => {
+      const height = "50px";
+
+      const wrapper = mount(<Banner height={height} />);
+
+      expect(wrapper.find("JSXStyle").props().children).toContain(
+        "height:50px;"
+      );
+    });
+
+    it("gives an error when height is not given as a string", () => {
+      const height = 50;
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Banner height={height} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("src prop", () => {});
+
+  describe("tint prop", () => {
+    it("applies tint class name given valid tint", () => {
+      const tint = "tertiary";
+
+      const wrapper = mount(<Banner tint={tint} />);
+
+      expect(
+        wrapper
+          .find("Banner")
+          .childAt(0)
+          .find("div")
+          .hasClass(tint)
+      ).toEqual(true);
+    });
+
+    it("gives an error when given an invalid tint", () => {
+      const tint = "hi hi hi";
+      const errors = jest.spyOn(console, "error").mockImplementation();
+
+      mount(<Banner tint={tint} />);
+
+      expect(errors).toHaveBeenCalledTimes(1);
+    });
+  });
+});
