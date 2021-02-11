776e50af40cb429553a0f8dabba1329ca7e6b61a
Carmen Li <carmen@MacBook-Pro.hitronhub.home>
2021-02-11 12:41:06 -0800

    tests for ButtonIcon component

--- packages/riipen-ui/src/components/ButtonIcon.jsx
+++ packages/riipen-ui/src/components/ButtonIcon.jsx
@@ -12,7 +12,6 @@ const ButtonIcon = props => {
     color,
     component,
     disabled,
-    outline,
     size,
     theme,
     ...other
@@ -58,116 +57,116 @@ const ButtonIcon = props => {
         </span>
       </Component>
       <style jsx>{`
-          .root {
-            background-color: transparent;
-            border: 0;
-            color: ${theme.palette.text.secondary};
-            cursor: pointer;
-            display: inline-flex;
-            padding: ${theme.spacing(2)}px;
-            position: relative;
-            ${outline ? "" : "outline: 0;"}
-            transition: all ${theme.transitions.duration.standard}ms;
-            user-select: none;
-          }
-          .root.focusVisible {
-            outline: 5px auto -webkit-focus-ring-color;
-          }
-          .root:active,
-          .root.focusVisible,
-          .root:hover {
-            background-color: transparent;
-            color: ${theme.palette.secondary.main};
-          }
-          .root:hover::before {
-            background-color: ${theme.palette.secondary.main};
-            border-radius: 50%;
-            bottom: 0;
-            content: "";
-            left: 0;
-            opacity: 0.1;
-            position: absolute;
-            right: 0;
-            top: 0;
-          }
+        .root {
+          background-color: transparent;
+          border: 0;
+          color: ${theme.palette.text.secondary};
+          cursor: pointer;
+          display: inline-flex;
+          padding: ${theme.spacing(2)}px;
+          position: relative;
+          outline: 0;
+          transition: all ${theme.transitions.duration.standard}ms;
+          user-select: none;
+        }
+        .root.focusVisible {
+          outline: 5px auto -webkit-focus-ring-color;
+        }
+        .root:active,
+        .root.focusVisible,
+        .root:hover {
+          background-color: transparent;
+          color: ${theme.palette.secondary.main};
+        }
+        .root:hover::before {
+          background-color: ${theme.palette.secondary.main};
+          border-radius: 50%;
+          bottom: 0;
+          content: "";
+          left: 0;
+          opacity: 0.1;
+          position: absolute;
+          right: 0;
+          top: 0;
+        }
 
-          a.root {
-            border: none;
-            text-decoration: none;
-          }
+        a.root {
+          border: none;
+          text-decoration: none;
+        }
 
-          .muted:hover {
-            color: ${theme.palette.text.primary};
-          }
+        .muted:hover {
+          color: ${theme.palette.text.primary};
+        }
 
-          .muted:hover::before {
-            background-color: ${theme.palette.grey[600]};
-          }
+        .muted:hover::before {
+          background-color: ${theme.palette.grey[600]};
+        }
 
-          .primary,
-          .primary:hover {
-            color: ${theme.palette.primary.main};
-          }
+        .primary,
+        .primary:hover {
+          color: ${theme.palette.primary.main};
+        }
 
-          .primary:hover::before {
-            background-color: ${theme.palette.primary.main};
-          }
-          .secondary,
-          .secondary:hover {
-            color: ${theme.palette.secondary.main};
-          }
-          .secondary:hover::before {
-            background-color: ${theme.palette.secondary.main};
-          }
-          .tertiary,
-          .tertiary:hover {
-            color: ${theme.palette.tertiary.main};
-          }
-          .tertiary:hover::before {
-            background-color: ${theme.palette.tertiary.main};
-          }
-          .white,
-          .white:hover {
-            color: ${theme.palette.common.white};
-          }
-          .white:hover::before {
-            background-color: ${theme.palette.common.white};
-          }
+        .primary:hover::before {
+          background-color: ${theme.palette.primary.main};
+        }
+        .secondary,
+        .secondary:hover {
+          color: ${theme.palette.secondary.main};
+        }
+        .secondary:hover::before {
+          background-color: ${theme.palette.secondary.main};
+        }
+        .tertiary,
+        .tertiary:hover {
+          color: ${theme.palette.tertiary.main};
+        }
+        .tertiary:hover::before {
+          background-color: ${theme.palette.tertiary.main};
+        }
+        .white,
+        .white:hover {
+          color: ${theme.palette.common.white};
+        }
+        .white:hover::before {
+          background-color: ${theme.palette.common.white};
+        }
 
-          .disabled {
-            opacity: 0.5;
-            pointer-events: none;
-          }
+        .disabled {
+          opacity: 0.5;
+          pointer-events: none;
+        }
 
-          .label {
-            align-items: center;
-            display: flex;
-            font-size: 18px;
-            justify-content: center;
-            text-align: center;
-            width: 100%;
-          }
+        .label {
+          align-items: center;
+          display: flex;
+          font-size: 18px;
+          justify-content: center;
+          text-align: center;
+          width: 100%;
+        }
 
-          .icon-small {
-            height: 16px;
-            width: 16px;
-          }
-          .icon-medium {
-            height: 18px;
-            width: 18px;
-          }
-          .icon-large {
-            height: 20px;
-            width: 20px;
-          }
+        .icon-small {
+          height: 16px;
+          width: 16px;
+        }
+        .icon-medium {
+          height: 18px;
+          width: 18px;
+        }
+        .icon-large {
+          height: 20px;
+          width: 20px;
+        }
 
-          .font-small {
-            font-size: 16px;
-          }
-          .font-large {
-            font-size: 20px;
-          }
-        `}</style>
+        .font-small {
+          font-size: 16px;
+        }
+        .font-large {
+          font-size: 20px;
+        }
+      `}</style>
     </React.Fragment>
   );
 };
@@ -207,11 +206,6 @@ ButtonIcon.propTypes = {
   disabled: PropTypes.bool,
 
   /**
-   * If `true`, the button will have an outline when focussed. If `false` the button will not have an outline when focussed.
-   */
-  outline: PropTypes.bool,
-
-  /**
    * The size of the chip.
    */
   size: PropTypes.oneOf(["small", "medium", "large"]),
@@ -227,7 +221,6 @@ ButtonIcon.defaultProps = {
   classes: [],
   color: "default",
   disabled: false,
-  outline: false,
   size: "medium"
 };
 
