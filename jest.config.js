03ac52e7ec4dd2bbc313e1375acf2eb0e63fc095
Imogene McDonald <imogene@Imogenes-MBP.hitronhub.home>
2021-02-12 09:07:27 -0800

    remove config changes

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,5 @@ module.exports = {
   displayName: "riipen-ui",
   name: "riipen-ui",
   roots: ["<rootDir>/packages/riipen-ui"],
-  setupFilesAfterEnv: ["<rootDir>/packages/riipen-ui/test/setup.js"],
-  testRegex: "/*.test.jsx$",
-  collectCoverage: true,
-  coverageReporters: ["lcov"],
-  coverageDirectory: "test-coverage",
-  coverageThreshold: {
-   global: {
-   branches: 0,
-   functions: 0,
-   lines: 0,
-   statements: 0
-   }
-  },
-  moduleDirectories: ["node_modules", "src"]
+  setupFilesAfterEnv: ["<rootDir>/packages/riipen-ui/test/setup.js"]
 };
