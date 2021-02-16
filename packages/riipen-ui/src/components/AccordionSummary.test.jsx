4dd1755415cad55bf9fd25a4f40e83fbaf479b30
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2021-02-16 10:21:50 -0800

    [PLT-6623] Tests for AccordionSummary (#148)
    * Tests for AccordionSummary
    
    * adjusted icon test, merged with master to remove the onFocusVisible warning
    
    * additional test for iconProp classes
    
    Co-authored-by: Imogene McDonald <imogene@Imogenes-MBP.hitronhub.home>

---
+++ packages/riipen-ui/src/components/AccordionSummary.test.jsx
@@ -0,0 +1,100 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+import AccordionSummary from "./AccordionSummary";
+
+const Icon = <FontAwesomeIcon icon={faChevronDown} />;
+
+describe("<AccordionSummary>", () => {
+  it("renders without errors", () => {
+    let error;
+
+    try {
+      mount(<AccordionSummary />);
+    } catch (theError) {
+      error = theError;
+    }
+
+    expect(error).toEqual(undefined);
+  });
+
+  it("renders correct snapshot", () => {
+    const wrapper = mount(<AccordionSummary />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("sets correct default props", () => {
+    const wrapper = mount(<AccordionSummary />);
+
+    expect(wrapper.find("AccordionSummary").props().classes).toEqual([]);
+    expect(wrapper.find("AccordionSummary").props().iconProps).toEqual({});
+  });
+
+  describe("children prop", () => {
+    it("displays given children", () => {
+      const child = <div>A child</div>;
+
+      const wrapper = mount(<AccordionSummary>{child}</AccordionSummary>);
+
+      expect(wrapper.containsMatchingElement(child)).toBeTruthy();
+    });
+  });
+
+  describe("classes prop", () => {
+    it("sets custom class", () => {
+      const classes = ["newClass"];
+
+      const wrapper = mount(<AccordionSummary classes={classes} />);
+
+      expect(
+        wrapper
+          .find("AccordionSummary")
+          .childAt(0)
+          .hasClass(classes[0])
+      ).toEqual(true);
+    });
+  });
+
+  describe("icon prop", () => {
+    it("renders icon given valid custom icon prop", () => {
+      const icon = Icon;
+
+      const wrapper = mount(<AccordionSummary icon={icon} />);
+
+      expect(
+        wrapper.find("AccordionSummary").containsMatchingElement(icon)
+      ).toEqual(true);
+    });
+  });
+
+  describe("iconProps prop", () => {
+    it("passes given iconProp attributes to icon prop", () => {
+      const icon = Icon;
+      const iconProps = { size: "small" };
+
+      const wrapper = mount(
+        <AccordionSummary icon={icon} iconProps={iconProps} />
+      );
+
+      expect(wrapper.find("ButtonIcon").props().size).toEqual(iconProps.size);
+    });
+
+    it("passes iconProp classes to icon classes", () => {
+      const icon = Icon;
+      const iconProps = { classes: ["small"] };
+
+      const wrapper = mount(
+        <AccordionSummary icon={icon} iconProps={iconProps} />
+      );
+
+      expect(wrapper.find("ButtonIcon").props().classes[0]).toContain(
+        iconProps.classes
+      );
+    });
+  });
+});
