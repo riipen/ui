5a822d772bb4927372a8afd84562a7fdcddbed69
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2021-03-02 09:40:46 -0800

    [PLT-6639] Tests for Form (#164)
    * Tests for Form
    
    * Added tests checking that errors are rendered
    
    * removed test in errors prop
    
    Co-authored-by: Imogene McDonald <imogene@Imogenes-MBP.hitronhub.home>

---
+++ packages/riipen-ui/src/components/Form.test.jsx
@@ -0,0 +1,239 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import Form from "./Form";
+import Typography from "./Typography";
+
+describe("<Form>", () => {
+  it("renders without errors", () => {
+    let error;
+
+    try {
+      mount(<Form />);
+    } catch (theError) {
+      error = theError;
+    }
+
+    expect(error).toEqual(undefined);
+  });
+
+  it("renders correct snapshot", () => {
+    const wrapper = mount(<Form />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  describe("default props", () => {
+    it("sets correct default props", () => {
+      const wrapper = mount(<Form />);
+
+      expect(wrapper.find("Form").props().enter).toEqual(true);
+      expect(wrapper.find("Form").props().errorScroll).toEqual(true);
+    });
+  });
+
+  describe("children prop", () => {
+    it("displays given children", () => {
+      const child = <div>child</div>;
+
+      const wrapper = mount(<Form>{child}</Form>);
+
+      expect(wrapper.containsMatchingElement(child)).toBeTruthy();
+    });
+  });
+
+  describe("classes prop", () => {
+    it("sets custom class", () => {
+      const classes = ["newClass"];
+
+      const wrapper = mount(<Form classes={classes} />);
+
+      expect(
+        wrapper
+          .find("Form")
+          .childAt(0)
+          .hasClass(classes[0])
+      ).toEqual(true);
+    });
+
+    it("appends higher order values to classes prop with withClass decorator", () => {
+      const classes = ["riipen", "riipen-form"];
+
+      const wrapper = mount(<Form />);
+
+      expect(wrapper.find("Form").props().classes).toEqual(classes.sort());
+    });
+  });
+
+  describe("error prop", () => {
+    const props = {};
+
+    const tests = [
+      { argument: "Error" },
+      { argument: <Typography>Please provide a name</Typography> }
+    ];
+
+    tests.forEach(test => {
+      it("sets class name as error with valid custom error type", () => {
+        props.error = test.argument;
+
+        const wrapper = mount(<Form error={props.error} />);
+
+        expect(
+          wrapper
+            .find("Notice")
+            .childAt(0)
+            .childAt(0)
+            .hasClass("error")
+        ).toEqual(true);
+      });
+    });
+
+    it("sets class name as errorContainer when valid error is used", () => {
+      const error = "Yay";
+
+      const wrapper = mount(<Form error={error} />);
+
+      expect(
+        wrapper
+          .find("Form")
+          .childAt(0)
+          .hasClass("errorContainer")
+      ).toEqual(true);
+    });
+
+    it("renders error", () => {
+      const error = "Yay";
+
+      const wrapper = mount(<Form error={error} />);
+
+      expect(wrapper.find("Form").text()).toEqual(error);
+    });
+
+    it("renders error within Typography element if error is a string", () => {
+      const error = "Yay";
+
+      const wrapper = mount(<Form error={error} />);
+
+      expect(wrapper.find("Typography").contains(error)).toEqual(true);
+    });
+
+    it("renders error as given element", () => {
+      const error = <Typography>Please provide a name</Typography>;
+
+      const wrapper = mount(<Form error={error} />);
+
+      expect(wrapper.find("Typography").contains(error)).toEqual(false);
+      expect(wrapper.find("Form").props().error.type).toEqual(Typography);
+    });
+  });
+
+  describe("errors prop", () => {
+    const props = {};
+
+    const tests = [
+      { argument: ["Error", "Sad"] },
+      { argument: { errorOne: "Error", errorTwo: "Sad" } },
+      {
+        argument: [
+          <Typography>Please provide a name</Typography>,
+          <Typography>Error</Typography>
+        ]
+      }
+    ];
+
+    tests.forEach(test => {
+      it("sets class name as errors with valid custom errors type", () => {
+        props.errors = test.argument;
+
+        const wrapper = mount(<Form errors={props.errors} />);
+
+        expect(
+          wrapper
+            .find("Notice")
+            .childAt(0)
+            .childAt(0)
+            .hasClass("errors")
+        ).toEqual(true);
+      });
+    });
+
+    it("sets class name as errorContainer when valid errors are used", () => {
+      const errors = ["Error", "Sad"];
+
+      const wrapper = mount(<Form errors={errors} />);
+
+      expect(
+        wrapper
+          .find("Form")
+          .childAt(0)
+          .hasClass("errorContainer")
+      ).toEqual(true);
+    });
+
+    it("does not set errorContainer class name without valid errors", () => {
+      const wrapper = mount(<Form />);
+
+      expect(
+        wrapper
+          .find("Form")
+          .childAt(0)
+          .hasClass("errorContainer")
+      ).toEqual(false);
+    });
+
+    it("renders errors from given string array", () => {
+      const errors = ["Error", "Sad"];
+
+      const wrapper = mount(<Form errors={errors} />);
+
+      expect(
+        wrapper
+          .find(".errors")
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .text()
+      ).toEqual(errors[0]);
+      expect(
+        wrapper
+          .find(".errors")
+          .childAt(1)
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .text()
+      ).toEqual(errors[1]);
+    });
+
+    it("renders errors from given object", () => {
+      const errors = {
+        Error: "Please provide more cheese",
+        Warning: "Minimum cheese added"
+      };
+
+      const wrapper = mount(<Form errors={errors} />);
+
+      expect(
+        wrapper
+          .find(".errors")
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .text()
+      ).toEqual("Error: Please provide more cheese");
+      expect(
+        wrapper
+          .find(".errors")
+          .childAt(1)
+          .childAt(0)
+          .childAt(0)
+          .childAt(0)
+          .text()
+      ).toEqual("Warning: Minimum cheese added");
+    });
+  });
+});
