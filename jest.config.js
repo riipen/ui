0af156ea93b2fa240220941df09b4cbe80a2c068
Stephen Tredger <stredger@gmail.com>
2021-03-03 17:34:50 -0800

    Only test files in the src dir with jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: "riipen-ui",
   name: "riipen-ui",
-  roots: ["<rootDir>/packages/riipen-ui"],
-  setupFilesAfterEnv: ["<rootDir>/packages/riipen-ui/test/setup.js"]
+  roots: ["<rootDir>/packages/riipen-ui/src"],
+  setupFilesAfterEnv: ["<rootDir>/packages/riipen-ui/test/setup.js"],
 };
