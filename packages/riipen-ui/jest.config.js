170634e7ded1d725c9c6fd3692221be6cba886e8
Stephen Tredger <stephen.tredger@riipen.com>
2021-03-04 09:03:03 -0800

    Fix build and update dependiecies (#166)
    * Fix build and update dependiecies
    
    * Only test files in the src dir with jest
    
    * Fix dependency warnings
    
    * bust circleci npm cache
    
    * revert peer dependency change in riipen-ui-docs
    
    * Update npm required engine

--- packages/riipen-ui/jest.config.js
+++ packages/riipen-ui/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   displayName: "riipen-ui",
   name: "riipen-ui",
-  roots: ["<rootDir>/packages/riipen-ui"],
+  roots: ["<rootDir>/packages/riipen-ui/src"],
   rootDir: "../..",
   setupFilesAfterEnv: ["<rootDir>/packages/riipen-ui/test/setup.js"]
 };
