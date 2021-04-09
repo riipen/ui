a6cdb939628449a82fe1ef0726b8bed844b9d6a2
Kiersten Mort <kiersten.mort@riipen.com>
2021-04-09 16:30:20 -0700

    Fix micon margin to end

--- packages/riipen-ui-docs/src/pages/components/input/Adornments.jsx
+++ packages/riipen-ui-docs/src/pages/components/input/Adornments.jsx
@@ -25,32 +25,16 @@ export default function Adornments() {
       </div>
       <div style={section}>
         <Input
-          id="name"
-          label="Sports"
-          startAdornment={<FontAwesomeIcon icon={faQuidditch} />}
-          size="medium"
-        />
-      </div>
-      <div style={section}>
-        <Input
-          id="name"
-          startAdornment={<FontAwesomeIcon icon={faQuidditch} />}
-          label="Sports"
-          size="large"
-        />
-      </div>
-      <div style={{ marginBottom: "10px" }} />
-      <div style={section}>
-        <Input
           endAdornment={<FontAwesomeIcon icon={faQuidditch} />}
           id="name"
           label="Sports"
           size="small"
         />
       </div>
+      <div style={{ marginBottom: "10px" }} />
       <div style={section}>
         <Input
-          endAdornment={<FontAwesomeIcon icon={faQuidditch} />}
+          startAdornment={<FontAwesomeIcon icon={faQuidditch} />}
           id="name"
           label="Sports"
           size="medium"
@@ -58,8 +42,25 @@ export default function Adornments() {
       </div>
       <div style={section}>
         <Input
-          endAdornment={<FontAwesomeIcon icon={faQuidditch} />}
           id="name"
+          label="Sports"
+          endAdornment={<FontAwesomeIcon icon={faQuidditch} />}
+          size="medium"
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }} />
+      <div style={section}>
+        <Input
+          startAdornment={<FontAwesomeIcon icon={faQuidditch} />}
+          id="name"
+          label="Sports"
+          size="large"
+        />
+      </div>
+      <div style={section}>
+        <Input
+          id="name"
+          endAdornment={<FontAwesomeIcon icon={faQuidditch} />}
           label="Sports"
           size="large"
         />
