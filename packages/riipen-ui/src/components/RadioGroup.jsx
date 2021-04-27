9a0b5365bc95db76a66d8f1e0e1b363e33c30ffc
Stephen Tredger <stredger@gmail.com>
2021-04-27 09:02:26 -0700

    Update radio group to take value prop

--- packages/riipen-ui/src/components/RadioGroup.jsx
+++ packages/riipen-ui/src/components/RadioGroup.jsx
@@ -7,104 +7,118 @@ import withClasses from "../utils/withClasses";
 import InputLabel from "./InputLabel";
 import Typography from "./Typography";
 
-class RadioGroup extends React.Component {
-  static displayName = "RadioGroup";
-
-  static propTypes = {
-    /**
-     * The content of the component.
-     */
-    children: PropTypes.node,
-
-    /**
-     * An error to display below the input.
-     */
-    error: PropTypes.node,
-
-    /**
-     * Hint text to display under the label of the input.
-     */
-    hint: PropTypes.node,
-
-    /**
-     * Label text to display for the input.
-     */
-    label: PropTypes.node,
-
-    /**
-     * Callback fired when a radio button is selected.
-     *
-     * @param {object} event The event source of the callback.
-     * You can pull out the new value by accessing `event.target.value` (string).
-     */
-    onChange: PropTypes.func,
-
-    /**
-     * If true, an asterisk will be appended to the end of the label.
-     */
-    required: PropTypes.bool,
-
-    /**
-     * A warning to display below the input.
-     */
-    warning: PropTypes.node
-  };
-
-  handleChange = e => {
-    const { onChange } = this.props;
-
+const RadioGroup = ({
+  children,
+  error,
+  hint,
+  label,
+  name,
+  onChange,
+  required,
+  value,
+  warning,
+  ...other
+}) => {
+  const handleChange = e => {
     if (onChange) {
       onChange(e, e.target.value);
     }
   };
 
-  render() {
-    const {
-      children,
-      error,
-      hint,
-      label,
-      required,
-      warning,
-      ...other
-    } = this.props;
+  const childrenWithProps = React.Children.map(
+    children || [],
+    child =>
+      child &&
+      React.cloneElement(child, {
+        checked: value === child.props.value,
+        name: `${name}-${child.props.value}`,
+        onChange: handleChange,
+        ...other,
+        ...child.props
+      })
+  ).filter(Boolean);
 
-    const childrenWithProps = React.Children.map(children, child => {
-      if (!child) return null;
+  return (
+    <React.Fragment>
+      <fieldset>
+        {(label || hint) && (
+          <InputLabel hint={hint} htmlFor={name} required={required}>
+            {label}
+          </InputLabel>
+        )}
+        {childrenWithProps}
+        {error && (
+          <Typography color="negative" variant="body2">
+            {error}
+          </Typography>
+        )}
+        {warning && (
+          <Typography color="secondary" variant="body2">
+            {warning}
+          </Typography>
+        )}
+      </fieldset>
+      <style jsx>{`
+        fieldset {
+          border: none;
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
 
-      return React.cloneElement(child, { onChange: this.handleChange });
-    });
+RadioGroup.propTypes = {
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
 
-    return (
-      <React.Fragment>
-        <fieldset>
-          {(label || hint) && (
-            <InputLabel hint={hint} htmlFor={other.name} required={required}>
-              {label}
-            </InputLabel>
-          )}
-          {childrenWithProps}
-          {error && (
-            <Typography color="negative" variant="body2">
-              {error}
-            </Typography>
-          )}
-          {warning && (
-            <Typography color="secondary" variant="body2">
-              {warning}
-            </Typography>
-          )}
-        </fieldset>
-        <style jsx>{`
-          fieldset {
-            border: none;
-            margin: 0;
-            padding: 0;
-          }
-        `}</style>
-      </React.Fragment>
-    );
-  }
-}
+  /**
+   * An error to display below the input.
+   */
+  error: PropTypes.node,
+
+  /**
+   * Hint text to display under the label of the input.
+   */
+  hint: PropTypes.node,
+
+  /**
+   * Label text to display for the input.
+   */
+  label: PropTypes.node,
+
+  /**
+   * The name of the form element the label is bound to.
+   */
+  name: PropTypes.string,
+
+  /**
+   * Callback fired when a radio button is selected.
+   *
+   * @param {object} event The event source of the callback.
+   * You can pull out the new value by accessing `event.target.value` (string).
+   */
+  onChange: PropTypes.func,
+
+  /**
+   * If true, an asterisk will be appended to the end of the label.
+   */
+  required: PropTypes.bool,
+
+  /**
+   * The value of the selected radio button
+   */
+  value: PropTypes.any,
+
+  /**
+   * A warning to display below the input.
+   */
+  warning: PropTypes.node
+};
+
+RadioGroup.displayName = "RadioGroup";
 
 export default withClasses(RadioGroup);
