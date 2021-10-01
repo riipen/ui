3431d6fed2a6bc59d790e15bbda50c0303047811
Kyubin Han <kyubinhan11@gmail.com>
2021-10-01 12:43:24 -0700

    Don't let override event listeners in Input
    Passed onFocus & onBlur props should be called in handleFocus and handleBlur respectively

--- packages/riipen-ui/src/components/Input.jsx
+++ packages/riipen-ui/src/components/Input.jsx
@@ -18,6 +18,8 @@ const Input = ({
   labelProps,
   meta,
   multiline,
+  onBlur,
+  onFocus,
   required,
   size,
   startAdornment,
@@ -30,10 +32,12 @@ const Input = ({
   const { ref, isFocusVisible, onBlurVisible } = useIsFocusVisible();
 
   const handleFocus = e => {
+    if (onFocus) onFocus();
     setFocusVisible(isFocusVisible(e));
   };
 
   const handleBlur = () => {
+    if (onBlur) onBlur();
     setFocusVisible(false);
     onBlurVisible();
   };
@@ -288,6 +292,16 @@ Input.propTypes = {
    * If `true`, a textarea element will be rendered.
    */
   multiline: PropTypes.bool,
+
+  /**
+   * Function to execute when input is out of focus.
+   */
+  onBlur: PropTypes.func,
+
+  /**
+   * Function to execute when input is in focus.
+   */
+  onFocus: PropTypes.func,
 
   /**
    * If true, an asterisk will be appended to the end of the label.
